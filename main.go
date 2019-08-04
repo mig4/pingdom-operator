7a3c6c9876544a84e5463b2255a5c8eb958cc96e
mig4 <42650719@auril.club>
2019-08-04 16:11:22 +0100

    Add a `Check` resource boilerplate
    Generated with:
    `kubebuilder create api --group observability --version v1alpha1 --kind Check`

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ import (
 	"flag"
 	"os"
 
+	observabilityv1alpha1 "gitlab.com/mig4/pingdom-operator/api/v1alpha1"
+	"gitlab.com/mig4/pingdom-operator/controllers"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -35,6 +37,7 @@ var (
 func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
 
+	_ = observabilityv1alpha1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -58,6 +61,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.CheckReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Check"),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Check")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
